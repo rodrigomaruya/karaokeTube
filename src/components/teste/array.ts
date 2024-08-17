892b454f3cd7245609bcb8ccdfa25cf1d9e589f0
Rodrigo <rymaruya@gmail.com>
2024-08-17 15:00:20 +0900

    nova list

--- src/components/teste/array.ts
+++ src/components/teste/array.ts
@@ -8,29 +8,39 @@ export interface youTubeProps{
 export const youTube: youTubeProps[]=[
   {
   id: 1,
-  titulo: 'Sweet Child O Mine - Guns N Roses',
-  videoId: 'uhiamj_I7II'
+  titulo: 'Eu Juro - Leandro e Leonardo',
+  videoId: 'jgwjCaGLXLg'
   },
   {
     id: 2,
-    titulo: 'Caetano Veloso - Sozinho - Karaokê',
+    titulo: 'Sozinho - Caetano Veloso ',
     videoId: 'gktA6xDOnx0'
   },
   {
     id: 3,
-    titulo: 'Menina Veneno Karaokê',
+    titulo: 'Menina Veneno',
     videoId: '25hHYmowa38'
   },
   {
     id: 4,
-    titulo: 'Easy On Me - Adele',
-    videoId: 'M7Qg5H0luo0'
+    titulo: 'Evidências - Chitãozinho e Xororó',
+    videoId: 'TGXkdU9S0OU'
   },
   {
     id: 5,
-    titulo: 'Radiohead - Creep ',
-    videoId: '3WLy3AblvmQ'
+    titulo: 'Velha infância - Tribalistas ',
+    videoId: 'lDaLPv_InjE'
   },
+  {
+    id: 6,
+    titulo: 'ANNA JÚLIA - Los Hermanos',
+    videoId: 'd_sQIBh01GQ'
+  },
+  {
+    id: 7,
+    titulo: 'Só Hoje - Jota Quest',
+    videoId: 'imkvOWvB9fs'
+  }
 ]
 
 
